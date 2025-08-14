09093b60058b84d1abfdb651ab5a761efd6c9f47
ChaviIrom <chavi.irom@gmail.com>
2025-08-14 21:48:01 +0300

    change path for search pictures

--- project_react/src/component/Header/IconInputSearch.jsx
+++ project_react/src/component/Header/IconInputSearch.jsx
@@ -1,18 +1,8 @@
 import { useNavigate, useLocation } from "react-router-dom";
-import searchIcon from "../../../Public/Pictures/HomePage/icons8-search-50.png";
+import searchIcon from "../../../public/Pictures/HomePage/icons8-search-50.png";
 import '../../styles/DesignHeader.css'
 
 export default function IconSearchProduct() {
-
-// IconSearchProduct
-// console.log("Navigate with background:", location);
-
-// Root.jsx
-// console.log("Root location:", location);
-// const background = location.state && location.state.background;
-// console.log("Background in Root:", background);
-
-
   return (
     <div className="search-button-icon" style={{ cursor: 'pointer' }}>
       <img src={searchIcon} alt="חיפוש" width={35} />
