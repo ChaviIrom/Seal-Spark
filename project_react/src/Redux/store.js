8c1ba031ec36cab707f020df2b8ad9f6f439170b
ChaviIrom <chavi.irom@gmail.com>
2025-09-07 03:56:44 +0300

    fix: change Redux folder to lowercase redux

--- project_react/src/Redux/store.js
+++ project_react/src/Redux/store.js
@@ -1,5 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import {thunk} from 'redux-thunk';
+
 import shopCartReducer  from './reducers/shopCartReducer.js';
 import usersReducer from './reducers/usersReducer.js'
 import productsReducer  from './reducers/productsReducer.js';
